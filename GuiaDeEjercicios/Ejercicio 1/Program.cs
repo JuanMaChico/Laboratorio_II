799fe563c6fd27b60544199dc5d5c16a340c9ef6
JuanMaChico <juanmanuelchico@hotmail.com>
2023-04-13 08:50:33 -0300

    2 ejercicios

---
+++ GuiaDeEjercicios/Ejercicio 1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Ingresar 5 números por consola, guardándolos en una variable escalar. Luego calcular y mostrar: el
+ * valor máximo, el valor mínimo y el promedio.
+ */
+Console.WriteLine("-----------Ejecicio 1-------------");
+Console.Write("Ingrese 5 números separados por espacios: ");
+int[] numeros = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int maximo = numeros.Max();
+int minimo = numeros.Min();
+double promedio = numeros.Average();
+
+Console.WriteLine("El número máximo es: {0}", maximo);
+Console.WriteLine("El número mínimo es: {0}", minimo);
+Console.WriteLine("El promedio es: {0}", promedio);
+
+Console.ReadLine();
---
+++ GuiaDeEjercicios/Ejercicio 2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ *  Ingresar un número y mostrar: el cuadrado y el cubo del mismo. Se debe validar que el número sea
+ *  mayor que cero, caso contrario, mostrar el mensaje: &quot;ERROR. ¡Reingresar número!&quot;.
+ *  Nota: Utilizar el método ‘Pow’ de la clase Math para realizar la operación.
+ */
+Console.WriteLine("------------- Ejecicio 2 -------------");
+
+int numero;
+bool esNumeroValido;
+
+do
+{
+    Console.Write("Ingrese un número mayor que cero: ");
+    string entrada = Console.ReadLine();
+
+    esNumeroValido = int.TryParse(entrada, out numero) && numero > 0;
+
+    if (!esNumeroValido)
+    {
+        Console.WriteLine("El número ingresado es inválido. Debe ser un número entero mayor que cero.");
+    }
+} while (!esNumeroValido);
+
+int cuadrado = numero * numero;
+int cubo = numero * numero * numero;
+
+Console.WriteLine("El cuadrado de {0} es: {1}", numero, cuadrado);
+Console.WriteLine("El cubo de {0} es: {1}", numero, cubo);
+
+Console.ReadLine();
