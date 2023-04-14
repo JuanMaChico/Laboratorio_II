961c51d94111c398ae933bf49e7e10a1b54daeec
JuanMaChico <juanmanuelchico@hotmail.com>
2023-04-14 10:55:39 -0300

    Ejercicio 3 + Guia

---
+++ GuiaDeEjercicios/Ejercicio 3/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Mostrar por pantalla todos los números primos que haya hasta el número que ingrese el usuario
+ * por consola.
+ * Nota: Utilizar estructuras repetitivas, selectivas y la función módulo (%).
+ */
+Console.WriteLine("------------- Ejecicio 3 -------------");
+
+Console.Write("Ingrese un número: ");
+int limite = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Números primos hasta {0}:", limite);
+
+for (int numero = 2; numero <= limite; numero++)
+{
+    bool esPrimo = true;
+
+    for (int divisor = 2; divisor <= Math.Sqrt(numero); divisor++)
+    {
+        if (numero % divisor == 0)
+        {
+            esPrimo = false;
+            break;
+        }
+    }
+
+    if (esPrimo)
+    {
+        Console.Write("{0} ", numero);
+    }
+}
+
+Console.ReadLine();
